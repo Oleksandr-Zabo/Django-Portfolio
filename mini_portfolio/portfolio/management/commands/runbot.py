3543b7a1fd38cd7385c603b3184d7b14c5724cc3
ZabOleksandr <zoleksandr06@gmail.com>
2026-06-18 19:06:07 +0300

    update folder

--- mini_portfolio/portfolio/management/commands/runbot.py
+++
@@ -1,8 +0,0 @@
-﻿from django.core.management.base import BaseCommand
-from telegram_bot.bot import bot
-
-class Command(BaseCommand):
-    help = "Запускає Telegram‑бота"
-
-    def handle(self, *args, **options):
-        bot.polling(none_stop=True)
